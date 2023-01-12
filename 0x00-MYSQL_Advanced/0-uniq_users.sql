c49ebda90e71ac072fc5a0593f517408c795a7ba
BenFaruna <benjaminfaruna@gmail.com>
2023-01-13 00:27:13 +0100

    Removed files

--- 0x00-MYSQL_Advanced/0-uniq_users.sql
+++
@@ -1,7 +0,0 @@
--- SQL query for creating table with id, email and name column
--- The id column is a primary key with auto increment
--- The email column only accepts unique values
-CREATE TABLE IF NOT EXISTS users
-    (id INTEGER NOT NULL AUTO_INCREMENT,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    name VARCHAR(255), PRIMARY KEY (id));
--- 0x00-MYSQL_Advanced/4-store.sql
+++
@@ -1,9 +0,0 @@
--- trigger that reduces the quantity of an item on insert
-DELIMITER $
-CREATE TRIGGER dec_quantity AFTER INSERT ON orders
-    FOR EACH ROW
-    BEGIN
-        UPDATE items SET quantity = quantity - NEW.number
-        WHERE NEW.item_name = name;
-    END;$
-DELIMITER ;
